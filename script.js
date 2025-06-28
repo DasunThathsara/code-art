47dfe00e4ffbeb8e9ec6d88f33f2b06bf097541e
DasunThathsara <dasun.thathsara.sri@gmail.com>
2025-06-28 10:28:18 +0530

    tea cup

--- script.js
+++ script.js
@@ -4,3 +4,26 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext("2d");
+
+
+function teaCup (startX, startY, colour, scale = 1) {
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+
+    ctx.lineTo(startX + 50 * scale, startY);
+    ctx.lineTo(startX + 50 * scale, startY + 5 * scale);
+    ctx.lineTo(startX, startY + 5 * scale);
+    ctx.closePath();
+
+    ctx.fillStyle = colour;
+    ctx.fill();
+    
+
+    ctx.beginPath();
+    ctx.arc(startX + 25 * scale, startY - 10 * scale, 15 * scale, Math.PI, 0, true);
+    ctx.fillStyle = colour;
+    ctx.fill();
+}
+
+
+teaCup(100, 200, "red", 2);
