c19c7f5633c4e1edc32d6a879f9059a274ebe685
DasunThathsara <dasun.thathsara.sri@gmail.com>
2025-06-28 10:58:51 +0530

    table

--- script.js
+++ script.js
@@ -6,24 +6,59 @@ canvas.height = window.innerHeight;
 var ctx = canvas.getContext("2d");
 
 
-function teaCup (startX, startY, colour, scale = 1) {
+function teaCup (startX, startY, colour = "black", scale = 2) {
     ctx.beginPath();
     ctx.moveTo(startX, startY);
 
-    ctx.lineTo(startX + 50 * scale, startY);
-    ctx.lineTo(startX + 50 * scale, startY + 5 * scale);
+    ctx.lineTo(startX + 42 * scale, startY);
+    ctx.lineTo(startX + 42 * scale, startY + 5 * scale);
     ctx.lineTo(startX, startY + 5 * scale);
-    ctx.closePath();
 
     ctx.fillStyle = colour;
     ctx.fill();
-    
 
     ctx.beginPath();
-    ctx.arc(startX + 25 * scale, startY - 10 * scale, 15 * scale, Math.PI, 0, true);
+    ctx.arc(startX + 20 * scale, startY - 15 * scale, 15 * scale, Math.PI, 0, true);
     ctx.fillStyle = colour;
     ctx.fill();
 }
 
+function tableTop(startX, startY, colour = "black", scale = 1) {
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
 
-teaCup(100, 200, "red", 2);
+    ctx.lineTo(startX + 180 * scale, startY);
+    ctx.lineTo(startX + 100 * scale, startY + 50 * scale);
+    ctx.lineTo(startX - 80 * scale, startY + 50 * scale);
+    ctx.lineTo(startX, startY);
+    ctx.closePath();
+
+    ctx.fillStyle = colour;
+    ctx.fill();
+}
+
+function tableLeg(startX, startY, colour = "black", scale = 1){
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    
+    ctx.lineTo(startX * scale, startY + 150 * scale);
+    ctx.lineTo(startX +  5 * scale, startY + 150 * scale);
+    ctx.lineTo(startX + 5 * scale, startY + 50 * scale);
+    ctx.lineTo(startX * scale, startY + 50 * scale);
+
+    ctx.closePath();
+    ctx.fillStyle = colour;
+    ctx.fill();
+}
+
+function table (startX, startY, colour = "black", scale = 1) {
+    
+    tableTop(startX, startY, colour, scale);
+    tableLeg(startX, startY - 50, colour, scale);
+    tableLeg(startX + 180, startY - 50, colour, scale);
+    tableLeg(startX - 80 * scale, startY, colour, scale);
+    tableLeg(startX + 100 * scale, startY, colour, scale);
+}
+
+
+table(100, 200);
