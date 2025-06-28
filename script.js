e46c52b3d55be4f2412140364ef25f34462f55d1
Ravindu-Hasanka <rhatu2000@gmail.com>
2025-06-28 10:35:54 +0530

    feat: main ellips with and without shadow

--- script.js
+++ script.js
@@ -4,3 +4,30 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 var ctx = canvas.getContext("2d");
+
+
+function drawMainEllipseWithShadow(x, y, radiusX, radiusY, rotation, color, shadowColor) {
+    ctx.beginPath();
+    ctx.ellipse(x+2, y + 5, radiusX, radiusY, rotation, 0, 2 * Math.PI);
+    ctx.fillStyle = shadowColor;
+    ctx.fill();
+    ctx.beginPath();
+    ctx.ellipse(x, y, radiusX, radiusY, rotation, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+function drawMainEllipseWithoutShadow(x, y, radiusX, radiusY, rotation, color) {
+    ctx.beginPath();
+    ctx.ellipse(x, y, radiusX, radiusY, rotation, 0, 2 * Math.PI);
+    ctx.fillStyle = color;
+    ctx.fill();
+}
+
+function drawMainArt(){
+    drawMainEllipseWithoutShadow(450, 230, 180, 400, Math.PI / 2, "rgba(120, 120, 120, 1)");
+    drawMainEllipseWithoutShadow(1050, 230, 180, 400, Math.PI / 2, "rgba(120, 120, 120, 1)");
+    drawMainEllipseWithoutShadow(750, 500, 180, 400, Math.PI / 2, "rgba(120, 120, 120, 1)");
+}
+
+drawMainArt()
